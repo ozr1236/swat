5f80e1eab5ce7a3b328468d7d3affb17a4099f05
ozr1236 <88008273+ozr1236@users.noreply.github.com>
2021-07-31 18:20:24 +0300

    Update SWAT Helper.lua

--- SWAT Helper.lua
+++ SWAT Helper.lua
@@ -1,7 +1,7 @@
 script_name('SWAT Helper')
 script_description('Удобный помощник для Автошколы.')
 script_author('JustMini')
-script_version_number(42)
+script_version_number(43)
 script_version('1.1')
 script_dependencies('imgui; samp events; lfs')
 
